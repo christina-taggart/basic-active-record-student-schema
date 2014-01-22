b0768658d8e9fda7ae33754a39b8c14564bf75dc
Alexander Leishman <null>
2014-01-22 15:43:12 -0800

    completed basic input validation

--- source/ar-student-schema/app/models/student.rb
+++ source/ar-student-schema/app/models/student.rb
@@ -1,6 +1,14 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
+  validates :email,
+            :format => {
+              :with    => /^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})$/i,
+              :message => "Only letters allowed" }
+  validates :email, :uniqueness => true
+  validate :age_cannot_be_less_than_five
+  validate :phone_number_must_have_at_leat_ten_digits
+
   def name
     self.first_name + " " + self.last_name
   end
@@ -8,8 +16,20 @@ class Student < ActiveRecord::Base
   def age
     ((Date.today - self.birthday)/(365.2422)).to_i
   end
+
+  def age_cannot_be_less_than_five
+    if age < 5
+      errors.add(:birthday, "Age cannot be less than 5 years")
+    end
+  end
+
+  def phone_number_must_have_at_leat_ten_digits
+    if self.phone.scan(/\d/).count < 10
+      errors.add(:phone, "Phone number must have at least 10 digits")
+    end
+  end
 end
 
-student = Student.all[0]
-p student.name
-p student.age
+# student = Student.all[0]
+# p student.name
+# p student.age
