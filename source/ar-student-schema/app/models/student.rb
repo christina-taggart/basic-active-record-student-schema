5cc18f8d81f555179b1126a80f7a4c8207ed3657
Emily Foley & Patrick Vilhena <pair+EmFoley.nifengle@devbootcamp.com>
2014-01-22 15:58:39 -0800

    Release 1 concludes. Name and age accessible. Rspec tests pass.

--- source/ar-student-schema/app/models/student.rb
+++ source/ar-student-schema/app/models/student.rb
@@ -1,5 +1,16 @@
 require_relative '../../db/config'
-
+require 'date'
 class Student < ActiveRecord::Base
-# implement your Student model here
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    now = Time.now.utc.to_date
+    now.year - self.birthday.year - ((now.month > self.birthday.month || (now.month == self.birthday.month && now.day >= self.birthday.day)) ? 0 : 1)
+  end
 end
+
+student = Student.new(first_name: 'Bob', last_name: 'Saget', birthday: Date.new(1984,3,22))
+p student.age
