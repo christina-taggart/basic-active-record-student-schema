1b0e953840c9a32bd7e3b279c73c50b380e0e817
John Olmsted <null>
2014-01-22 15:34:41 -0800

    Add #name and #age

--- source/ar-student-schema/app/models/student.rb
+++ source/ar-student-schema/app/models/student.rb
@@ -1,5 +1,20 @@
 require_relative '../../db/config'
+require 'date'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+  def name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def age
+    now = Time.now.to_date
+    now.year - self.birthday.year - (bday_already? ? 1 : 0)
+  end
+
+  private
+
+  def bday_already?
+    now = Time.now.to_date
+    self.birthday.to_date.change(:year => now.year) > now
+  end
 end
