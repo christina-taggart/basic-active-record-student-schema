64fece6c508ba805d8cc8a00ca2f6526c499324f
David Goodman <null>
2014-01-22 17:11:03 -0800

    finished all releases, by myself, very proud and you should be impressed

--- source/ar-student-schema/app/models/student.rb
+++ source/ar-student-schema/app/models/student.rb
@@ -1,16 +1,16 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-  # attr_reader :first_name, :last_name, :birthday
-  # def initialize(args = {})
-  #   @first_name = args[:first_name]
-  #   @last_name = args[:last_name]
-  #   @gender = args[:gender]
-  #   @birthday = args[:birthday]
-  #   @email = args[:email]
-  #   @phone = args[:phone]
+   validates :email, :uniqueness => true, :format => { :with => /.\@.+\.../, :message => "Not a valid email"}
+   #validates :email, :format => { :with => /.\@.+\.../, :message => "Not a valid email"}
+   validate :person_must_be_5_years_old
+   validates :phone, :format => { :with => /.\d{3}.\s\d{3}.\d{4}\s.\d{4}/, :message => "Not a valid phone number"}
 
-  # end
+  def person_must_be_5_years_old
+     if self.age < 5
+      errors.add(:birthday, "This person is too young, must be over 5")
+    end
+  end
 
   def name
     self.first_name + " " + self.last_name
@@ -18,8 +18,8 @@ class Student < ActiveRecord::Base
 
   def age
     2014 - self.birthday.year - 1
-   #now.year - @student.birthday.year - ((now.month > @student.birthday.month || (now.month == @student.birthday.month && now.day >= @student.birthday.day))
   end
+
 end
 
 # vals = {first_name: "David", last_name: "Goodman", gender: "male", birthday: 1989, email: "thisisemail@something.com", phone: "627-383-7125"}
