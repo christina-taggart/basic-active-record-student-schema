e74613ae17109475649240795899aa80649e9667
Apprentice <apprentice@dbc25.local>
2014-01-22 17:02:18 -0800

    Release 2

--- source/ar-student-schema/app/models/student.rb
+++ source/ar-student-schema/app/models/student.rb
@@ -1,5 +1,34 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+
+  validates :email, :uniqueness => true
+  validates :email,
+            :format => {
+              :with    => /\A([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})\z/i,
+              :message => "Invalid email" }
+  validate :older_than_five
+  validate :phone_digits
+
+  def name
+    self.first_name + " " + self.last_name
+  end
+
+  def age
+    ((Date.today - self.birthday) / (365.25)).to_i
+  end
+
+  def older_than_five
+    errors.add(:age, "must be older than five") if age < 5
+  end
+
+  def phone_digits
+    errors.add(:birthday, "Number must be 10 digits") if self.phone.scan(/\d/).count < 10
+  end
+
+
 end
+
+
+
+
