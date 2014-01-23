9b7c7a4f982c5aab4a14765b90db1bc99067128e
Christina Taggart & Ricardo A. Rubio <pair+christina-taggart.rickarubio@devbootcamp.com>
2014-01-22 17:13:19 -0800

    Release 1: Made Object Relational Model for student name and age

--- source/ar-student-schema/app/models/student.rb
+++ source/ar-student-schema/app/models/student.rb
@@ -1,5 +1,14 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+  def name
+    "#{self[:first_name]} #{self[:last_name]}"
+  end
+  def age
+    age = Date.today.year - self[:birthday].year # 44
+    if Date.today.month <= self[:birthday].month
+      age -= 1
+    end
+    age
+  end
 end
