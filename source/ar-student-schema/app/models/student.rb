1b3b93abf364e1911e80b3c8f7d7df5c248d11ad
Darcey Lachtman <null>
2014-01-22 18:12:14 -0800

    Release 2 Advanced. Phone numbers checked if valid. All tests return true.

--- source/ar-student-schema/app/models/student.rb
+++ source/ar-student-schema/app/models/student.rb
@@ -3,6 +3,7 @@ require_relative '../../db/config'
 class Student < ActiveRecord::Base
   validates :email, :format => { :with => /[\d\w]+@[\d\w]+\.\w{2,}/ }, :uniqueness => true
   validates_numericality_of :age, {:greater_than_or_equal_to => 5}
+  validates :phone, :format => { :with => /\D*\d{3}\D*\d{3}\D*\d{4}/ }
   def name
     self.first_name << " " << self.last_name
   end
