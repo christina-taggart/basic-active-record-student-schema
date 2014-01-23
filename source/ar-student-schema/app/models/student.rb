50573e978d100261a0b262ac1f6c171e478f3002
David Goodman <null>
2014-01-22 16:17:47 -0800

    Release 0 and 1 done

--- source/ar-student-schema/app/models/student.rb
+++ source/ar-student-schema/app/models/student.rb
@@ -1,5 +1,28 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+  # attr_reader :first_name, :last_name, :birthday
+  # def initialize(args = {})
+  #   @first_name = args[:first_name]
+  #   @last_name = args[:last_name]
+  #   @gender = args[:gender]
+  #   @birthday = args[:birthday]
+  #   @email = args[:email]
+  #   @phone = args[:phone]
+
+  # end
+
+  def name
+    self.first_name + " " + self.last_name
+  end
+
+  def age
+    2014 - self.birthday.year - 1
+   #now.year - @student.birthday.year - ((now.month > @student.birthday.month || (now.month == @student.birthday.month && now.day >= @student.birthday.day))
+  end
 end
+
+# vals = {first_name: "David", last_name: "Goodman", gender: "male", birthday: 1989, email: "thisisemail@something.com", phone: "627-383-7125"}
+# me = Student.new(vals)
+# p me.name
+# p me.age
--- source/ar-student-schema/db/migrate/20121011144238_create_students.rb
+++ source/ar-student-schema/db/migrate/20121011144238_create_students.rb
@@ -1,9 +1,35 @@
 require_relative '../config'
 
-# this is where you should use an ActiveRecord migration to 
+# this is where you should use an ActiveRecord migration to
 
 class CreateStudents < ActiveRecord::Migration
-  def change
-    # HINT: checkout ActiveRecord::Migration.create_table
-  end
+    def up
+    create_table :students do |x|
+      x.string :first_name
+      x.string :last_name
+      x.string :gender
+      x.date :birthday
+      x.string :email
+      x.string :phone
+
+      x.datetime :created_at
+      x.datetime :updated_at
+        end
+      end
+
+      def change
+        change_table :students do |z|
+
+      z.string :first_name
+      z.string :last_name
+      z.string :gender
+      z.date :birthday
+      z.string :email
+      z.string :phone
+
+      z.datetime :created_at
+      z.datetime :updated_at
+        end
+      end
+
 end
