36af3ca386c028b109ec9f7daadbb45e837fa0d5
John Olmsted <null>
2014-01-22 16:23:09 -0800

    Add email & age validations

--- source/ar-student-schema/app/models/student.rb
+++ source/ar-student-schema/app/models/student.rb
@@ -1,7 +1,21 @@
 require_relative '../../db/config'
 require 'date'
 
+
+class AgeValidator < ActiveModel::Validator
+  def validate(student)
+    if student.age < 5
+      student.errors[:base] << "Student must be at least 5 years old"
+    end
+  end
+end
+
+
 class Student < ActiveRecord::Base
+  validates :email, :format => { :with => /.+@.+\..{2,}/ }
+  validates :email, :uniqueness => true
+  validates_with AgeValidator
+
   def name
     "#{self.first_name} #{self.last_name}"
   end
