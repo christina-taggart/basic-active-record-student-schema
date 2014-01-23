363b91156b315a309b518c66f9e28f0e4f9b1211
Apprentice <apprentice@dbc32.local>
2014-01-22 16:17:30 -0800

    Release 1: returns name and age

--- source/ar-student-schema/app/models/student.rb
+++ source/ar-student-schema/app/models/student.rb
@@ -2,4 +2,16 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+
+  def name
+    "#{self[:first_name]} #{self[:last_name]}"
+  end
+
+  def age
+    age = (Date.today - self[:birthday]).to_i
+    age/365.25.to_i
+  end
+
+
+
 end
