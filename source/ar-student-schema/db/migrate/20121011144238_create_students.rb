ff801b1a086999145a98be287bb5ff886b915ce4
Spencer Smitherman & Elliot Chalfant <pair+bigspencey.epchalfant@devbootcamp.com>
2014-01-22 17:32:36 -0800

    completed release 0

--- source/ar-student-schema/db/migrate/20121011144238_create_students.rb
+++ source/ar-student-schema/db/migrate/20121011144238_create_students.rb
@@ -1,9 +1,16 @@
 require_relative '../config'
 
-# this is where you should use an ActiveRecord migration to 
+# this is where you should use an ActiveRecord migration to
 
 class CreateStudents < ActiveRecord::Migration
   def change
-    # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :students do |t|
+      t.string :first_name, :null => false
+      t.string :last_name, :null => false
+      t.string :gender, :null => false
+      t.date :birthday, :null => false
+      t.string :email, :null => false
+      t.string :phone, :null => false
+    end
   end
 end
