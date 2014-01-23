9c7974e83d882e6d5700d8dc170df9ee9e3d358f
Darcey Lachtman <null>
2014-01-22 17:40:55 -0800

    release 1

--- source/ar-student-schema/app/models/student.rb
+++ source/ar-student-schema/app/models/student.rb
@@ -2,4 +2,12 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+
+  def name
+    self.first_name << " " << self.last_name
+  end
+
+  def age
+    (Date.today - birthday).to_i / 365
+  end
 end
