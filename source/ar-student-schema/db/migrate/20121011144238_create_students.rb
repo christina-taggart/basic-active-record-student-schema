9301bd25ecdc6b0c9fa1f68f185c5c695cc8cd50
Darcey Lachtman <null>
2014-01-22 17:12:25 -0800

    Release 0 done. Basic migration achieved. Much left to learn still.

--- source/ar-student-schema/db/migrate/20121011144238_create_students.rb
+++ source/ar-student-schema/db/migrate/20121011144238_create_students.rb
@@ -1,9 +1,15 @@
 require_relative '../config'
 
-# this is where you should use an ActiveRecord migration to 
+# this is where you should use an ActiveRecord migration to
 
 class CreateStudents < ActiveRecord::Migration
   def change
+    create_table :students do |t|
+      t.string :first_name, :last_name, :gender, :email, :phone
+      t.date :birthday
+
+      t.timestamps
+    end
     # HINT: checkout ActiveRecord::Migration.create_table
   end
 end
