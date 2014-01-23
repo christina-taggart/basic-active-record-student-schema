8f9976d868c0278ad3426f7b61d3623691c6b301
mh120888 <matthew.alan.higgins@gmail.com>
2014-01-22 18:02:47 -0800

    Release 2: passes all tests but one (for phone number) - validates email uniqueness, format, and student age.

--- source/ar-student-schema/app/models/student.rb
+++ source/ar-student-schema/app/models/student.rb
@@ -1,6 +1,13 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
+  validates :email, :format => { :with => /\A.+@.+\..{2,}\z/,
+    :message => "Email must be in proper format" }
+  validates :email, :uniqueness => true
+  validates :age, :exclusion => { :in => (0..5).to_a,
+    :message => "This student is too young" }
+  # validates :phone, :format => { :with => /\A\d{10}\z/,
+  #   :message => "Phone number must be 10 digits long" }
   def name
     self[:first_name]+ " " + self[:last_name]
   end
