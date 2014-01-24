ff6025f3ba42de8967faa2948996b779bbed23c7
Apprentice <apprentice@DBC-Infinity-2.local>
2014-01-23 22:11:45 -0800

    Release 2 finished. Reviewing really helped me. Really hope it sticks.

--- source/ar-student-schema/app/models/student.rb
+++ source/ar-student-schema/app/models/student.rb
@@ -2,11 +2,21 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+  validates :email, :format => { :with => /[\d\w]+@[\d\w]+\.\w{2,}/ }, :uniqueness => true
+  validate :age_check
+  validates :phone, :format => { :with => /\D*\d{3}\D*\d{3}\D*\d{4}/ }
+
   def name
     "#{self.first_name} #{self.last_name}"
   end
 
   def age
     ((Date.today - self.birthday)/365).to_i
+  end
+
+  def age_check
+    if age < 5
+      errors.add(:birthday, "wait a few years")
+    end
   end
 end
