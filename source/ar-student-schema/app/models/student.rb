3cf5086c1a2e1ee857121829bffb8f0c29a02e96
Apprentice <apprentice@DBC-Infinity-2.local>
2014-01-23 21:58:48 -0800

    Release 1 complete. Challenges are easier. One day taught me lots.

--- source/ar-student-schema/app/models/student.rb
+++ source/ar-student-schema/app/models/student.rb
@@ -2,4 +2,11 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+  def name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def age
+    ((Date.today - self.birthday)/365).to_i
+  end
 end
